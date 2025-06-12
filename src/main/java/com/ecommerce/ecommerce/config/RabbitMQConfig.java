b49420deb7a91b8ce2d4e284f1d49bad8700e02b
harshit <harshit.shambharkar@recykal.com>
2025-06-12 11:28:30 +0530

    adding rabbit mq config

---
+++ src/main/java/com/ecommerce/ecommerce/config/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package com.ecommerce.ecommerce.config;
+
+import com.ecommerce.ecommerce.constants.ConfigConstants;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue queue() {
+        return new Queue(ConfigConstants.QUEUE_NAME);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(ConfigConstants.TOPIC_NAME, true, false);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(ConfigConstants.ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+}
---
+++ src/main/java/com/ecommerce/ecommerce/constants/ConfigConstants.java
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerce.constants;
+
+public class ConfigConstants {
+
+    public static final String QUEUE_NAME = "ecommerce_queue";
+
+    public static final String TOPIC_NAME = "ecommerce_topic";
+
+    public static final String ROUTING_KEY = "ecommerce_route";
+
+}
