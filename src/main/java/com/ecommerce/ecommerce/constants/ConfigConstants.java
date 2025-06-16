27b8a4fbaeea4849271475f3ad37df8e3387d899
harshit <harshit.shambharkar@recykal.com>
2025-06-16 12:13:29 +0530

    config const rename

--- src/main/java/com/ecommerce/ecommerce/constants/ConfigConstants.java
+++ src/main/java/com/ecommerce/ecommerce/constants/ConfigConstants.java
@@ -2,10 +2,10 @@ package com.ecommerce.ecommerce.constants;
 
 public final class ConfigConstants {
 
-    public static final String QUEUE_NAME = "ecommerce_queue";
+    public static final String QUEUE_NAME = "audit.log.queue";
 
-    public static final String TOPIC_NAME = "ecommerce_topic";
+    public static final String TOPIC_NAME = "audit.log.topic";
 
-    public static final String ROUTING_KEY = "ecommerce_route";
+    public static final String ROUTING_KEY = "audit.log.route.key";
 
 }
